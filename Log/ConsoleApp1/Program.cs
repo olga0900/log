b819dbdb1d0f92b227f74de4d8ed1f6c77221f5e
olga0900 <LizunovaOV@yandex.ru>
2023-03-11 00:20:12 +0300

    log

---
+++ Log/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+
+            Log.Information("пользователь вводит деньги");
+
+            var curs = 30;
+            var tryparse = false;
+            var value=0;
+            while (!tryparse)
+            {
+                Console.Write("Сколько у вас долларов: ");
+                var dollar = Console.ReadLine();
+                tryparse = int.TryParse(dollar, out value);
+                if (!tryparse)
+                {
+                    Log.Error("пользователь ошибся :(");
+                }
+            }
+            Log.Information("пользователь ввёл {0}", value); 
+            var rubli = value * curs;
+            Console.WriteLine("Курс: 1 доллар = " + curs+" Р");
+            if(value<500)
+            {
+                Console.WriteLine("Учитывая коммисию 0,8 p, у вас: " + (rubli-8)+" p");
+            }
+            else
+            {
+                Console.WriteLine("Учитывая коммисию 3%, у вас: " + (rubli - ((rubli * 0.8) / 100)));
+            }
+            Log.Warning("операция выполнена");
+            Log.CloseAndFlush();
+        }
+    }
+}
